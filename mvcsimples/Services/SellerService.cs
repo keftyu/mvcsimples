c21c4ce63f39cac5f1aa5003aef09d182dae8876
Kevin <kevin.yy@hotmail.com>
2023-04-17 00:16:42 -0300

    projeto iniciado

---
+++ mvcsimples/Services/SellerService.cs
@@ -0,0 +1,22 @@
+﻿using mvcsimples.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mvcsimples.Services
+{
+    public class SellerService
+    {
+        private readonly mvcsimplesContext _context;
+
+        public SellerService(mvcsimplesContext context)
+        {
+            _context = context;
+        }
+        public List<Seller> FindAll()
+        {
+            return _context.Seller.ToList();
+        }
+    }
+}
